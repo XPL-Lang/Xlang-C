83f5476cb4224df8b3e7b7073c979b21ff598e67
Tyler887 <tylermageeshields@gmail.com>
2022-02-09 18:08:05 +0000

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -1,1 +1,13 @@
+import os
+
 print("XLang 0.0.1 Alpha")
+print("type `help@' for more info")
+while True:
+  request = input(">>> ")
+  if request.startswith("PRINT >"):
+    print = request.replace("PRINT  >","",1)
+    print(print)
+  elif request.startswith("external@"):
+    com = request.replace("external@","",1)
+    import os
+    os.system(com
