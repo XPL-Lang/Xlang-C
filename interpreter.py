716d6835c8ad2a853076b73d4c866b2e2bdbc77a
Tyler887 <tylermageeshields@gmail.com>
2022-03-09 22:49:43 +0000

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -1,13 +1,16 @@
 import os
 
-print("XLang 0.0.1 Alpha")
+print("XLang Developer Release")
 print("type `help@' for more info")
 while True:
-  request = input(">>> ")
-  if request.startswith("PRINT >"):
-    print = request.replace("PRINT  >","",1)
+  request = input("xlang>")
+  if request.startswith("+"):
+    print = request.replace("PRINT>","",1)
     print(print)
   elif request.startswith("external@"):
     com = request.replace("external@","",1)
+    print(f"> {com}")
     import os
     os.system(com)
+  else:
+    print(f"Error: undefined Xscript '{request}'")
